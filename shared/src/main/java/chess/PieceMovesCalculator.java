c6c9d101c8c3697b1231180217e02dbf23bede46
anthonyphasay <aphasay@outlook.com>
2026-05-03 00:01:57 -0600

    added PieceMovesCalculator. Added public Bishop class that moves the bishop, check for next spot and color

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,43 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PieceMovesCalculator {
+
+    public Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition)
+    {
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[][] directions = {{1,1}, {1,-1}, {-1,1}, {-1,-1}};
+        ChessPiece currentPosition = board.getPiece(myPosition);
+
+        for(int[] d:directions)
+        {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            while (true)
+            {
+                row += d[0];
+                col += d[1];
+                if (row < 1 || row > 8|| col < 1 || col > 8)
+                {
+                    break;
+                }
+                ChessPosition nextPosition = new ChessPosition(row, col);
+                ChessPiece takenSpace = board.getPiece(nextPosition);
+                if (takenSpace == null)
+                {
+                    moves.add(new ChessMove(myPosition, nextPosition, null));
+                } else
+                {
+                    if(takenSpace.getTeamColor() != currentPosition.getTeamColor())
+                    {
+                        moves.add(new ChessMove(myPosition, nextPosition, null));
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
+    }
+}
